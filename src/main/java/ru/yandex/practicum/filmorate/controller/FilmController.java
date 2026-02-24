e300612f53b92fec9acf88873a3f10f5c0de040a
Елена Истомина <69IstominaEA@region.cbr.ru>
2026-02-24 10:48:16 +0300

    небольшой фикс для запуска проверок

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -25,6 +25,7 @@ import jakarta.validation.Valid;
 @RequestMapping("/films")
 public class FilmController {
 	private final Map<Long, Film> films = new HashMap<>();
+	
 	private final static Logger log = LoggerFactory.getLogger(FilmController.class);
 	
 	@GetMapping
