22bdcdac6a7271722687d4028204fd7f1cc5f984
Истомина Елена <69istominaea@region.cbr.ru>
2026-03-26 10:14:42 +0300

    fix по проверке

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -14,7 +14,7 @@ public class InMemoryUserStorage implements UserStorage {
     private final Map<Long, User> users;
 
     @Autowired
-    public InMemoryUserStorage () {
+    public InMemoryUserStorage() {
         this.users = new HashMap<>();
     }
 
@@ -72,7 +72,7 @@ public class InMemoryUserStorage implements UserStorage {
         Optional<User> optUser = find(id);
         Optional<User> optOtherUser = find(otherUserId);
 
-        if (optUser.isPresent() && optOtherUser.isPresent()){
+        if (optUser.isPresent() && optOtherUser.isPresent()) {
             User user = optUser.get();
             User otherUser = optOtherUser.get();
 
@@ -132,7 +132,7 @@ public class InMemoryUserStorage implements UserStorage {
         throw new NotFoundException(String.format("Пользователь с id=%s не найден.", newUser.getId()));
     }
 
-    public Optional<User> findById(Long userId){
+    public Optional<User> findById(Long userId) {
         return Optional.ofNullable(users.get(userId));
     }
 
@@ -145,7 +145,7 @@ public class InMemoryUserStorage implements UserStorage {
         return ++currentMaxId;
     }
 
-    private Optional<User> find(Long id){
+    private Optional<User> find(Long id) {
         return users.values().stream()
                 .filter(film -> film.getId().equals(id))
                 .findFirst();
