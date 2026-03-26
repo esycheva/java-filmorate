916bd03844b1a9973db6c005550c9d6a146d3e2f
Истомина Елена <69istominaea@region.cbr.ru>
2026-03-27 00:07:34 +0300

    remover unused import

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -8,8 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
-
 import ru.yandex.practicum.filmorate.model.User;
 
 import org.slf4j.Logger;
