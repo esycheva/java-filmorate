592f359172b4be449d1cd5d345f99b6ee91aa8e3
Истомина Елена <69istominaea@region.cbr.ru>
2026-03-26 09:55:59 +0300

    fix по результатам проверки

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -9,9 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.RecordNotValidException;
 
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import org.slf4j.Logger;
@@ -56,7 +54,7 @@ public class UserController {
 	}
 
 	@PutMapping("/{id}/friends/{friendId}")
-	public ResponseEntity<User> addFriend(@PathVariable Long id, @PathVariable Long friendId){
+	public ResponseEntity<User> addFriend(@PathVariable Long id, @PathVariable Long friendId) {
 		Optional<User> optUser = service.addToFriends(id, friendId);
 
 		return optUser.map(user -> ResponseEntity
@@ -76,7 +74,7 @@ public class UserController {
 	}
 
 	@GetMapping("/{id}/friends")
-	public List<User> showFriends(@PathVariable Long id){
+	public List<User> showFriends(@PathVariable Long id) {
 		List<User> friends = storage.showFriends(id);
 		if (friends.isEmpty()) {
 			throw new NotFoundException("Пользователь не найден.");
