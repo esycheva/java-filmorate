16c1dcc029c7da000ab28b0d091f98f48523abf8
Истомина Елена <69istominaea@region.cbr.ru>
2026-03-28 03:11:46 +0300

    fix checks

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Collection;
