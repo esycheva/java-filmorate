b099e157847713223a5ed73bf6264e0fcc020c12
Истомина Елена <69istominaea@region.cbr.ru>
2026-04-18 05:53:48 +0300

    Fix checks

--- src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
@@ -3,9 +3,7 @@ package ru.yandex.practicum.filmorate.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Mpa;
-import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.MpaStorage;
 
 import java.util.Collection;
