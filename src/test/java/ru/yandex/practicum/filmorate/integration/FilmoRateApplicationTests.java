3c05661adf0421afec65558e1b9e8ee05be21b55
Истомина Елена <69istominaea@region.cbr.ru>
2026-04-18 06:11:09 +0300

    fix checks

--- src/test/java/ru/yandex/practicum/filmorate/integration/FilmoRateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/integration/FilmoRateApplicationTests.java
@@ -21,7 +21,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 @JdbcTest
 @AutoConfigureTestDatabase
