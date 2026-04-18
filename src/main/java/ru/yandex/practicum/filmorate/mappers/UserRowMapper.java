5323ff81ddc682fb8ae13d46e1fa1fabb3c2a581
Истомина Елена <69istominaea@region.cbr.ru>
2026-04-18 05:48:38 +0300

    fix checks

--- src/main/java/ru/yandex/practicum/filmorate/mappers/UserRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mappers/UserRowMapper.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.mappers;
 
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.sql.ResultSet;
