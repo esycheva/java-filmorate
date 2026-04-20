720cdae3735d5af1d65473eedb748b9b59124164
Истомина Елена <69istominaea@region.cbr.ru>
2026-04-20 03:09:22 +0300

    fix checks

--- src/main/java/ru/yandex/practicum/filmorate/storage/MpaDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/MpaDbStorage.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.mappers.MpaRowMapper;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
-import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Optional;
 
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
@@ -16,7 +16,6 @@ import ru.yandex.practicum.filmorate.model.Friend;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.sql.Date;
-import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
