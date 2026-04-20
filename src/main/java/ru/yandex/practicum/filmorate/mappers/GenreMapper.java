2bae50bc926364d34f6e38aea44f25e687843ad5
Истомина Елена <69istominaea@region.cbr.ru>
2026-04-20 15:32:22 +0300

    fix checks

--- src/main/java/ru/yandex/practicum/filmorate/mappers/GenreMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mappers/GenreMapper.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface GenreMapper {
     GenreDto toDto(Genre genre);
-    
+
     Genre toEntity(GenreDto genreDto);
 
     List<GenreDto> toDtoList(Collection<Genre> genres);
